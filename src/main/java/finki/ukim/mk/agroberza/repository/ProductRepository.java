5068f8942e8066384a84d0c51455c675ecfd0b91
Jana Markovikj <jana.markovikj@endava.com>
2022-04-01 12:30:54 +0200

    repo annotations added

--- src/main/java/finki/ukim/mk/agroberza/repository/ProductRepository.java
+++ src/main/java/finki/ukim/mk/agroberza/repository/ProductRepository.java
@@ -3,7 +3,9 @@ package finki.ukim.mk.agroberza.repository;
 import finki.ukim.mk.agroberza.model.Product;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
     Optional<Product> findProductByName(String name);
 }
--- src/main/java/finki/ukim/mk/agroberza/repository/UserRepository.java
+++ src/main/java/finki/ukim/mk/agroberza/repository/UserRepository.java
@@ -2,6 +2,8 @@ package finki.ukim.mk.agroberza.repository;
 
 import finki.ukim.mk.agroberza.model.MainUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<MainUser, Long> {
 }
